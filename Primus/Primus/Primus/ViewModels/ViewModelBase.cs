5fb631be5d5d2db4cb02770337dce1fc85e4a96a
Michele Scandura <michele.scandura+copilot@lexisnexisrisk.com>
2024-07-14 15:43:17 +0200

    whoops

---
+++ Primus/Primus/Primus/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Primus.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Primus/Primus/Primus/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Primus.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
